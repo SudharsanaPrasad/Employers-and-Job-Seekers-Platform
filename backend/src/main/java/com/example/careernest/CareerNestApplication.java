c79a55c8b990a870f9ab12ff11a6befa687076fe
SudharsanaPrasad <sudharsanaprasadgs@gmail.com>
2026-07-24 10:00:00 +0530

    Initial commit

---
+++ backend/src/main/java/com/example/careernest/CareerNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.careernest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerNestApplication.class, args);
+	}
+
+}
